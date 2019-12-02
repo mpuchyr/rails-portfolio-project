dc2433a3b6c7963c4c089833063a84eb41e52ac0
mpuchyr <matt.puchyr@gmail.com>
2019-12-02 17:05:20 -0600

    Add functionality to log in via Facebook, as well as log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,28 @@
 class SessionsController < ApplicationController
 
     def create
-        
+        @user = User.find_by(email: auth['info']['email'])
+        if @user
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            @user = User.create do |u|
+                u.username = auth['info']['email'].split('@').first
+                u.email = auth['info']['email']
+                u.password = SecureRandom.hex
+            end
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to root_path
+    end
+
+    private
+    def auth
+        request.env['omniauth.auth']
     end
 end
