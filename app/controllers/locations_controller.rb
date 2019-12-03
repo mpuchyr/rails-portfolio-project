6dcacf7b7cd34e557894dc4dadceb62ec60010a3
mpuchyr <matt.puchyr@gmail.com>
2019-12-03 11:53:00 -0600

    Fix error in new location form by adding conditional logic to locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,14 +6,18 @@ class LocationsController < ApplicationController
     end
 
     def create
-        # binding.pry
-        @location = Location.find_or_create_by(name: location_params[:name])
+        binding.pry
+        if location_params[:name].blank?
+            @location = Location.find_by(id: location_params[:id])
+        else
+            @location = Location.find_or_create_by(name: location_params[:name])
+        end
         @photoshoot = @location.photoshoots.create(location_params[:photoshoots_attributes]["0"])
         redirect_to photoshoot_path(@photoshoot)
     end
 
     private
     def location_params
-        params.require(:location).permit(:name, photoshoots_attributes: [:start_time, :end_time, :comments, :user_id])
+        params.require(:location).permit(:name, :id, photoshoots_attributes: [:start_time, :end_time, :comments, :user_id])
     end
 end
