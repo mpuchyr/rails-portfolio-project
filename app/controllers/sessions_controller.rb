e149dfc9ef552ab07840a77bacd4cda63712af05
mpuchyr <matt.puchyr@gmail.com>
2019-12-02 20:47:33 -0600

    Fix bug where session[:user_id] was not created when a user logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
         else
             @user = User.find_by(username: params[:username])
             if @user && @user.authenticate(params[:password])
-                session[:user_id]
+                session[:user_id] = @user.id
                 redirect_to user_path(@user)
             else
                 flash[:alert] = "Username or password was incorrect"
