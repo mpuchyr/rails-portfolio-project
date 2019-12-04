5a3512b5fe1d0f47c753637993befd9b604d7220
mpuchyr <matt.puchyr@gmail.com>
2019-12-03 18:28:28 -0600

    Add redirect to photoshoots_controller if user not logged in, there is no photoshoot, or photoshoot does not belong to user

--- app/controllers/photoshoots_controller.rb
+++ app/controllers/photoshoots_controller.rb
@@ -1,6 +1,14 @@
 class PhotoshootsController < ApplicationController
 
     def show
-        @photoshoot = Photoshoot.find_by(id: params[:id])
+        if session[:user_id]
+            @photoshoot = Photoshoot.find_by(id: params[:id])
+            if !@photoshoot || @photoshoot.user.id != session[:user_id]
+                redirect_to user_path(session[:user_id])
+            end
+        else
+            redirect_to login_path
+        end
+
     end
 end
