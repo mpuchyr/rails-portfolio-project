5f418f6c0d3518e8eca715a6fe266e29724b4986
mpuchyr <matt.puchyr@gmail.com>
2019-12-04 16:31:57 -0600

    Add validation to photoshoot model preventing user from creating a photoshoot on a past date

--- app/models/photoshoot.rb
+++ app/models/photoshoot.rb
@@ -2,5 +2,13 @@ class Photoshoot < ApplicationRecord
     belongs_to :user
     belongs_to :location
 
+    validate :new_photoshoot_date_cannot_be_in_the_past
+
     accepts_nested_attributes_for :location
+
+    def new_photoshoot_date_cannot_be_in_the_past
+        if start_time < Date.today
+            errors.add(:photoshoot_date, "can't be in the past")
+        end
+    end
 end
