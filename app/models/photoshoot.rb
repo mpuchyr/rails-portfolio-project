3370a6690356651f9fbb5ac827268f3a94d6e88f
mpuchyr <matt.puchyr@gmail.com>
2019-12-04 16:35:46 -0600

    Add validation to prevent user from creating a photoshoot with the same start and end time

--- app/models/photoshoot.rb
+++ app/models/photoshoot.rb
@@ -2,13 +2,19 @@ class Photoshoot < ApplicationRecord
     belongs_to :user
     belongs_to :location
 
-    validate :new_photoshoot_date_cannot_be_in_the_past
+    validate :new_photoshoot_date_cannot_be_in_the_past, :photoshoot_cannot_start_and_end_at_same_time
 
     accepts_nested_attributes_for :location
 
     def new_photoshoot_date_cannot_be_in_the_past
         if start_time < Date.today
             errors.add(:photoshoot_date, "can't be in the past")
+        end
+    end
+
+    def photoshoot_cannot_start_and_end_at_same_time
+        if start_time == end_time
+            errors.add(:photoshoot_start_time, "can't be the same as end time")
         end
     end
 end
