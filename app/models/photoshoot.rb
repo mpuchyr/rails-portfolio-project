ea50983175aba302f155f0a6493522d407a36931
mpuchyr <matt.puchyr@gmail.com>
2019-12-04 16:44:23 -0600

    Add validation to prevent user from creating a photoshoot when they already have one at that time

--- app/models/photoshoot.rb
+++ app/models/photoshoot.rb
@@ -2,7 +2,8 @@ class Photoshoot < ApplicationRecord
     belongs_to :user
     belongs_to :location
 
-    validate :new_photoshoot_date_cannot_be_in_the_past, :photoshoot_cannot_start_and_end_at_same_time
+    validate :new_photoshoot_date_cannot_be_in_the_past, :photoshoot_cannot_start_and_end_at_same_time,
+        :cannot_have_multiple_photoshoots_at_the_same_time
 
     accepts_nested_attributes_for :location
 
@@ -15,6 +16,13 @@ class Photoshoot < ApplicationRecord
     def photoshoot_cannot_start_and_end_at_same_time
         if start_time == end_time
             errors.add(:photoshoot_start_time, "can't be the same as end time")
+        end
+    end
+
+    def cannot_have_multiple_photoshoots_at_the_same_time
+        shoot = Photoshoot.find_by(start_time: start_time)
+        if shoot
+            errors.add(:you_already_have_a_photoshoot, "scheduled at that time")
         end
     end
 end
