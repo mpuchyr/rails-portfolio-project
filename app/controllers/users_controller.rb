e435d0b03088d18566dbadb84a11f4cd8252c153
mpuchyr <matt.puchyr@gmail.com>
2019-12-03 18:14:39 -0600

    Displays link to each of a users photoshoots on the user's show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
     def show
-        @user = User.find_by(id: params[:id])
+        if session[:user_id]
+            @user = User.find_by(id: params[:id])
+            @photoshoots = @user.photoshoots.all
+        else
+            redirect_to root_path
+        end
     end
 
     def new
